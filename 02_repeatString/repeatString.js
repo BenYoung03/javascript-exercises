01a063575338ce896283974f828277fbf4eca2ec
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-08 20:39:20 -0400

    Finished helloWorld exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, numTimes) {
+    let string;
+    for(let i = 0; i < numTimes; i++){
+        
+    }
 };
 
 // Do not edit below this line
