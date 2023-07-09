b147d82ceee2fffcfd4d257283ace8f70747ef23
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-08 21:06:04 -0400

    Completed exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(start < 0 || end < 0) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+
+    if(end < start){
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+    for(start; start <= end; start++){
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
